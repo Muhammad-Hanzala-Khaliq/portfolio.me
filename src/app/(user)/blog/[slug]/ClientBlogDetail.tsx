87ef64a6c1f2a41a2fb5798a3aa80af7555e5e31
M.Hanzala Khaliq <hanzallahkhaliq@gmail.com>
2025-09-19 01:24:15 +0500

    Add Cache related feature

--- src/app/(user)/blog/[slug]/ClientBlogDetail.tsx
+++ src/app/(user)/blog/[slug]/ClientBlogDetail.tsx
@@ -24,7 +24,7 @@ export default function ClientBlogDetail({ slug }: { slug: string }) {
       try {
         const res = await fetch(
           `${process.env.NEXT_PUBLIC_BASE_URL}/api/blog/${slug}`,
-           { next: { revalidate:604800} }
+          { next: { revalidate: 60 } }
         );
         if (!res.ok) {
           console.error('Blog not found');
